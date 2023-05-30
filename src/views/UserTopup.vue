691b01e79820a2c449d08caad74bfdb8ed512d4c
yang <zy892065502@gmail.com>
2023-05-30 16:17:38 +0800

    Updated top-up page with QR code and loading animation

--- src/views/UserTopup.vue
+++ src/views/UserTopup.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="topup">
-      <h1>Top Up</h1>
+      <h1>充值</h1>
       <form @submit.prevent="topUp">
         <div>
-          <label for="amount">Amount:</label>
-          <input type="number" id="amount" min="0.01" step="0.01" v-model.number="amount" />
+          <label for="amount">金额:</label>
+          <input type="number" id="amount" min="1" step="1" v-model.number="amount" />
         </div>
-        <button type="submit">Top Up</button>
-        <button type="button" @click="backToChat">Back to Chat</button>
+        <div class="qrcode">
+          <img src="../../img/qrcode.jpg" alt="QR Code">
+        </div>
+        <button type="submit">
+          <span v-if="isLoading">
+            <div class="loading"></div>
+            校验中Loading
+          </span>
+          <span v-else>确认充值</span>
+        </button>
+        <button type="button" @click="backToChat">back</button>
       </form>
     </div>
   </template>
@@ -20,14 +29,18 @@
   export default {
     setup() {
       const store = useStore();
-      const amount = ref(0.0);
+      const amount = ref(1);
       const error = ref('');
-  
+      const isLoading = ref(false);
+
       const topUp = () => {
+        isLoading.value = true;
         store.dispatch('topUp', { amount: amount.value })
           .then(() => {
-            // 在成功充值后跳转到 userChat 页面
-            router.push('/userChat');
+            setTimeout(() => {
+              isLoading.value = false;
+              router.push('/userChat');
+            }, 8000); // 8 seconds
           })
           .catch(err => {
             error.value = 'Error in top up: ' + err.message;
@@ -38,7 +51,7 @@
         router.push('/userChat');
       }
   
-      return { amount, topUp, backToChat, error };
+      return { amount, topUp, backToChat, error, isLoading};
     },
   };
   </script>
@@ -87,5 +100,38 @@
   .topup button:hover {
     opacity:1;
   }
+  .qrcode {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+}
+
+.qrcode img {
+  max-width: 100%; 
+  height: auto;    
+}
+.loading {
+  display: inline-block;
+  vertical-align: middle;
+  border: 6px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 6px solid #3498db;
+  width: 20px;
+  height: 20px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin { /* Safari */
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
   </style>
   
